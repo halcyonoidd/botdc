46bb1383b0303d70c9e7996fbdb7694360aaa05a
halcyonoidd <agriazzukhruf1@gmail.com>
2026-02-08 10:52:35 +0700

    add help and ping command

--- commands/runs/main.go
+++ commands/runs/main.go
@@ -1,8 +1,8 @@
-package run
+package runs
 
 import (
 	"botdc/pkgs/help"
-	"botdc/pkgs/jtc"
+	// "botdc/pkgs/jtc"
 	"botdc/pkgs/ping"
 
 	"github.com/bwmarrin/discordgo"
@@ -12,9 +12,9 @@ func RunPing(s *discordgo.Session, i *discordgo.InteractionCreate) {
 	ping.GetPing(s, i)
 }
 
-func RunJTC(s *discordgo.Session, i *discordgo.InteractionCreate) {
-	jtc.GetJTC(s, i)
-}
+// func RunJTC(s *discordgo.Session, i *discordgo.InteractionCreate) {
+// 	jtc.GetJTC(s, i)
+// }
 
 func RunHelp(s *discordgo.Session, i *discordgo.InteractionCreate) {
 	help.GetHelp(s, i)
--- main.go
+++ main.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"os"
 
+	"botdc/commands/runs"
+
 	"github.com/bwmarrin/discordgo"
 	"github.com/joho/godotenv"
 )
@@ -20,10 +22,23 @@ func main() {
 		panic("Error creating Discord session: " + err.Error())
 	}
 
+	dg.AddHandler(func(s *discordgo.Session, i *discordgo.InteractionCreate) {
+		switch i.ApplicationCommandData().Name {
+		case "ping":
+			runs.RunPing(s, i) // Memanggil fungsi dari commands/runs/main.go
+		case "help":
+			runs.RunHelp(s, i) // Memanggil fungsi dari commands/runs/main.go
+		// case "jtc":           // Jika Anda ingin mengaktifkan JTC juga
+		// 	runs.RunJTC(s, i)
+		}
+	})
+
 	if err := dg.Open(); err != nil {
 		panic("Error opening connection: " + err.Error())
 	}
 	defer dg.Close()
+
+	RegisterCommands(dg)
 
 	fmt.Println("Bot is now running. Press CTRL-C to exit.")
 	select {}
@@ -42,15 +57,19 @@ func RegisterCommands(s *discordgo.Session) {
 			Name:        "help",
 			Description: "Get help with commands.",
 		},
+		{ // Jangan lupa daftarkan command jtc jika ingin dipakai
+			Name:        "jtc",
+			Description: "Join to create channel command",
+		},
 	}
-	
-	appID := s.State.User.ID // This is your application's ID
 
-	// Loop through and register each command
+	appID := s.State.User.ID
+
 	for _, cmd := range commands {
-		// Register the command globally (empty string for GuildID means global)
 		if _, err := s.ApplicationCommandCreate(appID, "", cmd); err != nil {
 			fmt.Println("Cannot create command:", cmd.Name, err)
+		} else {
+			fmt.Println("Command created:", cmd.Name)
 		}
 	}
 }
